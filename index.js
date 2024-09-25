e92b934533f3dde55fb99dad842aa731610ab171
ADITYA SINGH <singhaditya5096@gmail.com>
2024-09-26 05:21:56 +0530

    Your descriptive commit message

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ const pass = encodeURIComponent(process.env.MONGODB_PASSWORD);
 const connect = async () => {
     try {
         await mongoose.connect(`mongodb+srv://${username}:${pass}@cluster0.8ihgg.mongodb.net/registration`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
         });
         console.log("Connected to MongoDB");
     } catch (e) {
@@ -34,7 +32,7 @@ const Register = mongoose.model("Register", regSchema);
 app.use(body_parser.urlencoded({ extended: true }));
 app.use(body_parser.json());
 
-const port = process.env.PORT || 4008;
+const port = process.env.PORT || 3006;
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, 'pages', 'index.html'));
@@ -52,7 +50,7 @@ app.post("/register", async (req, res) => {
                 password
             });
             await regData.save();
-            res.redirect("/success");
+            res.redirect("/login");
         } else {
             res.redirect("/error"); // Handle existing user case
         }
@@ -69,7 +67,29 @@ app.get("/success", (req, res) => {
 app.get("/error", (req, res) => {
     res.sendFile(path.join(__dirname, 'pages', 'error.html'));
 });
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages', 'login.html'));
+});
+app.use(express.static(path.join(__dirname, 'home')));
 
+app.get("/home", (req, res) => {
+    res.sendFile(path.join(__dirname, 'home', 'home.html'));
+});
+//login
+app.post("/login", async(req,res)=>{
+    try{
+const email = req.body.email;
+const password= req.body.password;
+const useremail= await Register.findOne({email:email});
+if(useremail.password===password){
+    res.redirect("/home");
+}else{
+    alert("invalid username or password")
+}
+    }catch(e){
+        res.redirect("/error");
+    }
+})
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
